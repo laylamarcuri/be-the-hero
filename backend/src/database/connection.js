46b5f953cfa86546ae04e9825ec6987dbed679da
mlayla <laylamarcuri@gmail.com>
2020-05-15 14:53:03 -0300

    backend teste com jest

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require ('knex');
 const configurantion = require('../../knexfile');
 
+const config = process.env.NODE_ENV === 'test' ? configurantion.test : configurantion.development;
+
 const connetion = knex(configurantion.development);
 
 module.exports = connetion;
