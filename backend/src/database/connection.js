f0cba0c2753958a8bedb081993579eb41e89898c
mlayla <laylamarcuri@gmail.com>
2020-05-21 20:02:12 -0300

    final do the be hero

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -3,6 +3,6 @@ const configurantion = require('../../knexfile');
 
 const config = process.env.NODE_ENV === 'test' ? configurantion.test : configurantion.development;
 
-const connetion = knex(configurantion.development);
+const connetion = knex(config);
 
 module.exports = connetion;
